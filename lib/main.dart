c5b08b11c2ad84904c127a855db3cbecdfae71bf
Cesar Martino <cesar.martino@genialinvestimentos.com.br>
2020-04-28 20:53:47 -0300

    layout created

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=472f34ec";
 
-void main() async {
+void main() {
   runApp(MaterialApp(home: Home()));
 }
 
@@ -20,13 +20,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text){
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text){
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text){
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text("\$ Conversor \$"),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.blueGrey,
         centerTitle: true,
       ),
       body: FutureBuilder<Map>(
@@ -38,23 +64,56 @@ class _HomeState extends State<Home> {
               return Center(
                   child: Text(
                 "Carregando Dados...",
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
               ));
             default:
               if (snapshot.hasError) {
                 return Center(
                     child: Text(
                   "Erro ao Carregar Dados =(",
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
-                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
                 ));
               } else {
-                return Text("Dados carregados!", style: TextStyle(color: Colors.green),);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on,
+                            size: 150.0, color: Colors.blueGrey),
+                        buildTextField("Reais", "R\$", realController, _realChanged),
+                        Divider(),
+                        buildTextField("Dolares", "US\$", dolarController, _dolarChanged),
+                        Divider(),
+                        buildTextField("Euros", "€", euroController, _euroChanged),
+                      ]),
+                );
               }
           }
         },
       ),
     );
   }
+}
+
+Widget buildTextField(
+    String label, String prefix, TextEditingController controller, Function f) {
+  return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.blueGrey),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blueGrey)),
+          border: OutlineInputBorder(),
+          prefixText: prefix,
+          hintStyle: TextStyle(color: Colors.blueGrey),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white))),
+      style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+      onChanged: f,
+      keyboardType: TextInputType.number,);
 }
