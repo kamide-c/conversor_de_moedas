ee7eb02efce7b33189b533ff6039f78fcb66e92b
Cesar Martino <cesar.martino@genialinvestimentos.com.br>
2020-04-28 21:00:14 -0300

    prevents comma

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
@@ -20,7 +21,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final realController = TextEditingController();
   final dolarController = TextEditingController();
   final euroController = TextEditingController();
@@ -28,18 +28,18 @@ class _HomeState extends State<Home> {
   double dolar;
   double euro;
 
-  void _realChanged(String text){
-    if(text.isEmpty) {
+  void _realChanged(String text) {
+    if (text.isEmpty) {
       _clearAll();
       return;
     }
     double real = double.parse(text);
-    dolarController.text = (real/dolar).toStringAsFixed(2);
-    euroController.text = (real/euro).toStringAsFixed(2);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
   }
 
-  void _dolarChanged(String text){
-    if(text.isEmpty) {
+  void _dolarChanged(String text) {
+    if (text.isEmpty) {
       _clearAll();
       return;
     }
@@ -48,8 +48,8 @@ class _HomeState extends State<Home> {
     euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
-  void _euroChanged(String text){
-    if(text.isEmpty) {
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
       _clearAll();
       return;
     }
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
   }
 
-  void _clearAll(){
+  void _clearAll() {
     realController.text = "";
     dolarController.text = "";
     euroController.text = "";
@@ -102,11 +102,14 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Icon(Icons.monetization_on,
                             size: 150.0, color: Colors.blueGrey),
-                        buildTextField("Reais", "R\$", realController, _realChanged),
+                        buildTextField(
+                            "Reais", "R\$", realController, _realChanged),
                         Divider(),
-                        buildTextField("Dolares", "US\$", dolarController, _dolarChanged),
+                        buildTextField(
+                            "Dolares", "US\$", dolarController, _dolarChanged),
                         Divider(),
-                        buildTextField("Euros", "€", euroController, _euroChanged),
+                        buildTextField(
+                            "Euros", "€", euroController, _euroChanged),
                       ]),
                 );
               }
@@ -120,18 +123,20 @@ class _HomeState extends State<Home> {
 Widget buildTextField(
     String label, String prefix, TextEditingController controller, Function f) {
   return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-          labelText: label,
-          labelStyle: TextStyle(color: Colors.blueGrey),
-          enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueGrey)),
-          border: OutlineInputBorder(),
-          prefixText: prefix,
-          hintStyle: TextStyle(color: Colors.blueGrey),
-          focusedBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white))),
-      style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
-      onChanged: f,
-      keyboardType: TextInputType.numberWithOptions(decimal: true),);
+    controller: controller,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.blueGrey),
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.blueGrey)),
+        border: OutlineInputBorder(),
+        prefixText: prefix,
+        hintStyle: TextStyle(color: Colors.blueGrey),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white))),
+    style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+    onChanged: f,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+    inputFormatters: [BlacklistingTextInputFormatter(RegExp('[\\,]'))],
+  );
 }
